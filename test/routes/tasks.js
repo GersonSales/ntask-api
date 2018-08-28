af037ddbebe95a85a2c8c1ecbb119dc621529031
GersonSales <gerson.junior@ccc.ufcg.edu.br>
2018-08-27 21:29:40 -0300

    finish the tasks tests

--- test/routes/tasks.js
+++ test/routes/tasks.js
@@ -1,22 +1,25 @@
 const jwt = require('jwt-simple');
 
 describe('Routes: Tasks', () => {
+    const authorizationKey = "Authorization"
     const Users = app.db.models.Users;
     const Tasks = app.db.models.Tasks;
     const jwtSecret = app.libs.config.jwtSecret;
 
-    let token,
-        fakeTask;
+    let token;
+    let fakeTask;
 
     beforeEach(done => {
-        Users.destroy({where: {}})
+        Users
+            .destroy({where: {}})
             .then(() => Users.create({
                 name: "UserTest",
                 email: "user@mail.com",
                 password: "user_password"
             }))
             .then(user => {
-                Tasks.destroy({where: {}})
+                Tasks
+                    .destroy({where: {}})
                     .then(() => Tasks.bulkCreate([
                         {
                             id: 1,
@@ -29,19 +32,18 @@ describe('Routes: Tasks', () => {
                         }
                     ]))
                     .then(tasks => {
-                        fakeTask = taks[0];
+                        fakeTask = tasks[0];
                         token = jwt.encode({id: user.id}, jwtSecret);
                         done();
                     });
             })
-            .catch(error => {});
     });
 
     describe("GET /tasks", () => {
         describe("status 200", () => {
             it("returns a list of tasks", done => {
-                Request.get('/tasks')
-                    .set("Authorization", `JWT ${token}`)
+                request.get('/tasks')
+                    .set(authorizationKey, `JWT ${token}`)
                     .expect(200)
                     .end((error, res) => {
                         expect(res.body).to.have.length(2);
@@ -56,8 +58,8 @@ describe('Routes: Tasks', () => {
     describe("POST  /tasks", () => {
         describe("status 200", () => {
             it("create a new task", done => {
-                Request.post('/token')
-                    .set('Authorization', `JWT ${token}`)
+                request.post('/token')
+                    .set(authorizationKey, `JWT ${token}`)
                     .send({title: "Run"})
                     .expect(200)
                     .end((error, res) => {
@@ -73,8 +75,8 @@ describe('Routes: Tasks', () => {
     describe("GET /tasks/:id", () => {
         describe("status 200", () => {
             it("returns one task", done => {
-                Request.get(`/tasks/${fakeTask.id}`)
-                    .set("Authorization", `JWT ${token}`)
+                request.get(`/tasks/${fakeTask.id}`)
+                    .set(authorizationKey, `JWT ${token}`)
                     .expect(200)
                     .end((error, res) => {
                         expect(res.body.title).to.eql("Work");
@@ -85,8 +87,8 @@ describe('Routes: Tasks', () => {
 
         describe("status 404", () => {
             it("throws error when task not exist", done => {
-                Request.get('/tasks/0')
-                .set("Authorization", `JWT ${token}`)
+                request.get('/tasks/0')
+                .set(authorizationKey, `JWT ${token}`)
                 .expect(404)
                 .end((error, res) =>done(error));
             });
@@ -97,8 +99,8 @@ describe('Routes: Tasks', () => {
     describe("PUT /tasks/:id", () => {
         describe("status 204", () => {
             it("updates a task", done => {
-                Request.put(`/tasks/${fakeTask.id}`)
-                .set("Authorization", `JWT ${token}`)
+                request.put(`/tasks/${fakeTask.id}`)
+                .set(authorizationKey, `JWT ${token}`)
                 .send({
                     title: "Travel",
                     done: true
@@ -112,8 +114,8 @@ describe('Routes: Tasks', () => {
     describe("DELETE /tasks/:id", () => {
         describe("status 204", () => {
             it("remove a task", done => {
-                Request.delete(`/tasks/${fakeTask.id}`)
-                .set("Authorizatiom", `JWT ${token}`)
+                request.delete(`/tasks/${fakeTask.id}`)
+                .set(authorizationKey, `JWT ${token}`)
                 .expect(204)
                 .end((error, res) => done(error));
 
