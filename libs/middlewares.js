1c5229c4e87dc7965a7a7920d51f5b9bd171f7c2
GersonSales <gerson.junior@ccc.ufcg.edu.br>
2018-08-28 01:54:25 -0300

    create logger and cluster processor

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,8 +1,23 @@
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser'),
+    cors = require('cors'),
+    morgan = require('morgan'),
+    logger = require('./logger');
 
 module.exports = app => {
     app.set('port', 3000);
     app.set('json spaces', 4);
+    app.use(morgan("common", {
+        stream: {
+            write: (message) => {
+                logger.info(message)
+            }
+        }
+    }));
+    app.use(cors({
+        origin: ["http://localhost:3001"],
+        methods: ["GET", "POST", "PUT", "DELTE"],
+        allowedHeaders: ["Content-Type", "Authorization"]
+    }));
     app.use(bodyParser.json());
     app.use(app.auth.initialize());
     app.use((req, res, next) => {
